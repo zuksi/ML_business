699759f55d93d7384bdb75929dbd819f71a09a0b
zuksi <54374181+zuksi@users.noreply.github.com>
2020-12-05 20:14:37 +0300

    Add files via upload

---
+++ app/front/run_front_server.py
@@ -0,0 +1,59 @@
+import json
+
+from flask import Flask, render_template, redirect, url_for, request
+from flask_wtf import FlaskForm
+from requests.exceptions import ConnectionError
+from wtforms import IntegerField, SelectField, StringField
+from wtforms.validators import DataRequired
+
+import urllib.request
+import json
+
+class ClientDataForm(FlaskForm):
+    filename = StringField('VideoFile', validators=[DataRequired()])
+
+app = Flask(__name__)
+app.config.update(
+    CSRF_ENABLED=True,
+    SECRET_KEY='you-will-never-guess',
+)
+
+def get_prediction(filename):
+    body = {'filename': filename}
+
+    myurl = "http://0.0.0.0:8180/predict"
+    req = urllib.request.Request(myurl)
+    req.add_header('Content-Type', 'application/json; charset=utf-8')
+    jsondata = json.dumps(body)
+    jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
+    req.add_header('Content-Length', len(jsondataasbytes))
+    #print (jsondataasbytes)
+    response = urllib.request.urlopen(req, jsondataasbytes)
+    return json.loads(response.read())['predictions']
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/predicted/<response>')
+def predicted(response):
+    response = json.loads(response)
+    print(response)
+    return render_template('predicted.html', response=response)
+
+@app.route('/predict_form', methods=['GET', 'POST'])
+def predict_form():
+    form = ClientDataForm()
+    data = dict()
+    if request.method == 'POST':
+        data['filename'] = request.form.get('filename')
+        try:
+            response = str(get_prediction(data['filename']))
+            print(response)
+        except ConnectionError:
+            response = json.dumps({"error": "ConnectionError"})
+        return redirect(url_for('predicted', response=response))
+    return render_template('form.html', form=form)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8181, debug=True)
